2e1322204875e79206b9654043f85e90701a3f6d
Steven Lai <steven.lai@liricco.com>
2014-06-30 12:45:41 +0800

    After testing with timeout

--- test/packet/zigbee.py
+++ test/packet/zigbee.py
@@ -11,7 +11,8 @@ def gen_packet():
     zigbee_packet = zigbee.Packet(bytes(14 + len(zigbee_req.packet)), 0)
     zigbee_packet.mac_addr = 0x0102030405060708
     zigbee_packet.type = zigbee_packet.TYPE_ZB_ON
-    zigbee_packet.seq = (pkt_seq += 1)
+    zigbee_packet.seq = pkt_seq
+    pkt_seq += 1
     zigbee_packet.length = len(zigbee_req.packet)
     zigbee_packet.payload = zigbee_req.packet
     #print(zigbee_packet.packet)
@@ -23,13 +24,13 @@ def gen_packet():
     return pkt_packet.packet
     
 if __name__ == '__main__':
-    packet = gen_packet()
     from ...core.SerialSource import SerialSource
     from ...core import SerialProtocol
     SerialProtocol.DEBUG = True
     sf = SerialSource(None, '/dev/ttyS0:115200')
     sf.open()
     for i in range(0, 4):
+        packet = gen_packet()
         sf.writePacket(packet)
         print('Written', packet)
     for i in range(0, 4):
