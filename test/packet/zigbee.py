a616e9076b56ca60bd80de483fff5f85f942b163
Steven Lai <steven.lai@liricco.com>
2014-06-30 12:24:37 +0800

    Added codes for too many req test

--- test/packet/zigbee.py
+++ test/packet/zigbee.py
@@ -1,14 +1,17 @@
+pkt_seq = 0xE
+
 def gen_packet():
     from ...packet import zigbee_simple
     from ...packet import zigbee
     from ...packet import pkt
+    global pkt_seq
     zigbee_req = zigbee_simple.Packet(None, 0)
     zigbee_req.status = zigbee_req.ZB_REQ
     #print(zigbee_req.packet)
     zigbee_packet = zigbee.Packet(bytes(14 + len(zigbee_req.packet)), 0)
     zigbee_packet.mac_addr = 0x0102030405060708
     zigbee_packet.type = zigbee_packet.TYPE_ZB_ON
-    zigbee_packet.seq = 0xE
+    zigbee_packet.seq = (pkt_seq += 1)
     zigbee_packet.length = len(zigbee_req.packet)
     zigbee_packet.payload = zigbee_req.packet
     #print(zigbee_packet.packet)
@@ -26,9 +29,11 @@ if __name__ == '__main__':
     SerialProtocol.DEBUG = True
     sf = SerialSource(None, '/dev/ttyS0:115200')
     sf.open()
-    sf.writePacket(packet)
-    print('Written', packet)
-    packet = sf.readPacket()
-    print('Read', packet)
+    for i in range(0, 4):
+        sf.writePacket(packet)
+        print('Written', packet)
+    for i in range(0, 4):
+        packet = sf.readPacket()
+        print('Read', packet)
     sf.close()
     
